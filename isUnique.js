8deb42116b43e346704d79ca843df183f6982117
marineasiryan <marinasiryan14@gmail.com>
2022-08-14 01:37:48 +0400

    isUnique

--- isUnique.js
+++ isUnique.js
@@ -1,10 +1,14 @@
-function isUnique(str) {
-    for (let i = 0; i < str.length; i++) {
-        if (str.indexOf(str[i]) !== str.lastIndexOf(str[i])) {
+let arr = [];
+for (let i = 0; i < 255; i++) {
+    arr[i] = 0;
+}
+function check(str) {
+    for (let i = 0; i < str.length; ++i) {
+        if (arr[str.charCodeAt(i)] === 1) {
             return false;
         }
+        arr[str.charCodeAt(i)] = 1;
     }
     return true;
 }
-
-console.log(isUnique('hello'))
+console.log(check("tatul"));
